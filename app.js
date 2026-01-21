0ed24af3e431d11c39a9552f036c42638cb4ec1e
moch_fadhlil <fadhilahfadhil770@gmail.com>
2026-01-22 06:38:57 +0700

    halah jangar mangs

--- app.js
+++ app.js
@@ -14,19 +14,33 @@ function addTodo() {
     if(todoText.length > 0){
         allTodos.push(todoText);
         updateTodoList();
-        todoInput.value = '';
+        todoInput.value = "";
     }
 }
 function updateTodoList() {
-    todoListUL.innerHTML = '';
-    allTodos.forEach((todo, todoIndex)=> {
+    todoListUL.innerHTML = "";
+    allTodos.forEach((todo, todoIndex)=>{
         todoItem = createTodoItem(todo, todoIndex);
         todoListUL.append(todoItem);
     })
 }
-function createTodoItem(todo) {
+function createTodoItem(todo, todoIndex) {
+    const todoId = "todo-"+todoIndex;
     const todoLI = document.createElement("li");
-    todoLI.innerText = todo;
+    todoLI.className = "todo";
+    todoLI.innerHTML = `
+                <input type="checkbox" id="${todoId}">
+                <label for="${todoId}" class="custom-checkbox">
+                    <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
+                        <path d="m382-354 339-339q12-12 28-12t28 12q12 12 12 28.5T777-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/>
+                    </svg>
+                </label>
+                <label for="${todoId}" class="todo-text">
+                    ${todo}
+                </label>
+                <button class="delete-button">
+                    <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520q-17 0-28.5-11.5T160-760q0-17 11.5-28.5T200-800h160q0-17 11.5-28.5T400-840h160q17 0 28.5 11.5T600-800h160q17 0 28.5 11.5T800-760q0 17-11.5 28.5T760-720v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM400-280q17 0 28.5-11.5T440-320v-280q0-17-11.5-28.5T400-640q-17 0-28.5 11.5T360-600v280q0 17 11.5 28.5T400-280Zm160 0q17 0 28.5-11.5T600-320v-280q0-17-11.5-28.5T560-640q-17 0-28.5 11.5T520-600v280q0 17 11.5 28.5T560-280ZM280-720v520-520Z"/></svg>
+                </button>
+    `
     return todoLI;
 }
-
